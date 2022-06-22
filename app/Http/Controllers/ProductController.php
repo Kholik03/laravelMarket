847daba962be2b62d0e3bf07b1c0e37d164dda13
kholik03 <anurkholik03@gmail.com>
2022-06-22 23:57:05 +0700

    alpha

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Products;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public static function getDetail($id)
+    {
+        $data['product'] = Products::findById($id);
+
+        return view('front.product_detail',$data);
+    }
+}
